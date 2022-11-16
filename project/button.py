ee2bc5854b7f23e8a93525cb880efd1c8e97c89d
Jorge Gonzalez <jorgelg@usc.edu>
2022-11-15 16:37:36 -0800

    added get request to putton push

--- project/button.py
+++ project/button.py
@@ -1,5 +1,6 @@
 import time
 import grovepi
+import requests
 
 # Connect the Grove Button to digital port D3
 # SIG,NC,VCC,GND
@@ -13,6 +14,7 @@ while True:
         # Check for input
         if grovepi.digitalRead(PORT_BUTTON):
             deviceSelect = (deviceSelect + 1)%2
+            response = requests.get('http://127.0.0.1:5000/me/player/play')
         time.sleep(.5)
 
     except IOError:

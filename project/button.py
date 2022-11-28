365fde616a89548545dbc71cad1439c7c5863fec
Jorge Gonzalez <jorgelg@usc.edu>
2022-11-27 19:53:25 -0800

    move if statements

--- project/button.py
+++ project/button.py
@@ -9,16 +9,16 @@ import grove_rgb_lcd as lcd
 # Connect the Grove Button to digital port D3
 button = 3
 # Connect the Grove Green LED to digital port D4
-led_green = 4
+led_green = 2
 # Connect the Grove Red LED to digital port D5
-led_red = 5
+led_red = 4
 
-lcd.setRGB(0, 128, 0)
+#lcd.setRGB(0, 128, 0)
 
 # Setup
 grovepi.pinMode(button,"INPUT")
-pinMode(led_green,"OUTPUT")
-pinMode(led_red,"OUTPUT")
+grovepi.pinMode(led_green,"OUTPUT")
+grovepi.pinMode(led_red,"OUTPUT")
 deviceSelect = 0
 
 while True:
@@ -29,28 +29,28 @@ while True:
             # then device is switched play/paused
             deviceSelect = (deviceSelect + 1)%2
             if (deviceSelect==0):   # playing
-                response = requests.get('http://127.0.0.1:5000/me/player/play')
+                #response = requests.get('http://127.0.0.1:5000/me/player/play')
                 while (deviceSelect==0):
                     # red LED should be off
-                    digitalWrite(led_red,0)
+                    grovepi.digitalWrite(led_red,0)
                     # display title of selected song
                     setText("song title")
                     # blink green LED according to tempo of selected song
-                    digitalWrite(led_green,1)
+                    grovepi.digitalWrite(led_green,1)
                     # add tempo data (beats / min)              # TODOOOO
-                    tempo = 
+                    tempo = 149 #TEST NUMBER
                     time_between_blinks = 60.0 / tempo
                     time.sleep(time_between_blinks)
                     # Check if button is pressed
                     if grovepi.digitalRead(button):
                         (deviceSelect==0)
             elif (deviceSelect==1): # paused
-                response = requests.get('https://127.0.0.1:5000/me/player/pause')
+                #response = requests.get('https://127.0.0.1:5000/me/player/pause')
                 # green LED should be off
-                digitalWrite(led,0)
+                grovepi.digitalWrite(led_green,0)
                 # display "PAUSED" on LCD
                 setText("Spotify\nPAUSED")
-        time.sleep(.5)
+        time.sleep(1)
 
     except IOError:
         print ("Error")

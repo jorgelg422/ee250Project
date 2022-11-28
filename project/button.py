6e8e996546aeed40d8dec7d73b31c4d19d60c691
Jorge Gonzalez <jorgelg@usc.edu>
2022-11-27 19:56:48 -0800

    moved if statements

--- project/button.py
+++ project/button.py
@@ -28,28 +28,28 @@ while True:
         if grovepi.digitalRead(button):
             # then device is switched play/paused
             deviceSelect = (deviceSelect + 1)%2
-            if (deviceSelect==0):   # playing
-                #response = requests.get('http://127.0.0.1:5000/me/player/play')
-                while (deviceSelect==0):
-                    # red LED should be off
-                    grovepi.digitalWrite(led_red,0)
-                    # display title of selected song
-                    setText("song title")
-                    # blink green LED according to tempo of selected song
-                    grovepi.digitalWrite(led_green,1)
-                    # add tempo data (beats / min)              # TODOOOO
-                    tempo = 149 #TEST NUMBER
-                    time_between_blinks = 60.0 / tempo
-                    time.sleep(time_between_blinks)
-                    # Check if button is pressed
-                    if grovepi.digitalRead(button):
-                        (deviceSelect==0)
-            elif (deviceSelect==1): # paused
-                #response = requests.get('https://127.0.0.1:5000/me/player/pause')
-                # green LED should be off
-                grovepi.digitalWrite(led_green,0)
-                # display "PAUSED" on LCD
-                setText("Spotify\nPAUSED")
+        if (deviceSelect==0):   # playing
+            #response = requests.get('http://127.0.0.1:5000/me/player/play')
+            while (deviceSelect==0):
+                # red LED should be off
+                grovepi.digitalWrite(led_red,0)
+                # display title of selected song
+                setText("song title")
+                # blink green LED according to tempo of selected song
+                grovepi.digitalWrite(led_green,1)
+                # add tempo data (beats / min)              # TODOOOO
+                tempo = 149 #TEST NUMBER
+                time_between_blinks = 60.0 / tempo
+                time.sleep(time_between_blinks)
+                # Check if button is pressed
+                if grovepi.digitalRead(button):
+                    (deviceSelect==0)
+        elif (deviceSelect==1): # paused
+            #response = requests.get('https://127.0.0.1:5000/me/player/pause')
+            # green LED should be off
+            grovepi.digitalWrite(led_green,0)
+            # display "PAUSED" on LCD
+            setText("Spotify\nPAUSED")
         time.sleep(1)
 
     except IOError:

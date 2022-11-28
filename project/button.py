418141927e939e13c48570e4c17c16867e094284
Jackie Pham <jtpham@usc.edu>
2022-11-27 20:09:27 -0800

    lcd backlight colors and green lcd blinking

--- project/button.py
+++ project/button.py
@@ -13,7 +13,6 @@ led_green = 2
 # Connect the Grove Red LED to digital port D4
 led_red = 4
 
-#lcd.setRGB(0, 128, 0)
 
 # Setup
 grovepi.pinMode(button,"INPUT")
@@ -22,10 +21,11 @@ grovepi.pinMode(led_red,"OUTPUT")
 time.sleep(1)
 
 deviceSelect = 0
+greenOn = 0
+c = 0
 
 while True:
     try:
-
         # Check if button is pressed
         if grovepi.digitalRead(button):
             # then device is switched play/paused
@@ -38,11 +38,21 @@ while True:
                 # display title of selected song
                 setText("song title")
                 # blink green LED according to tempo of selected song
-                grovepi.digitalWrite(led_green,1)
+                if (greenOn == 0):      # if it is off, turn it on
+                    grovepi.digitalWrite(led_green,1)
+                elif (greenOn == 1):    # if it is on, turn it off
+                    grovepi.digitalWrite(led_green,0)
+                greenOn = (greenOn + 1)%2
                 # add tempo data (beats / min)              # TODOOOO
                 tempo = 149 #TEST NUMBER
                 time_between_blinks = 60.0 / tempo
                 time.sleep(time_between_blinks)
+                # lcd constantly changes colors if song is playing
+                setRGB(c,255-c,0)
+                if (c==254):
+                    c = 0
+                else:
+                    c = c + 1
                 # Check if button is pressed
                 if grovepi.digitalRead(button):
                     (deviceSelect==0)
@@ -50,6 +60,10 @@ while True:
             #response = requests.get('https://127.0.0.1:5000/me/player/pause')
             # green LED should be off
             grovepi.digitalWrite(led_green,0)
+            # red LED should be on
+            grovepi.digitalWrite(led_red,1)
+            # lcd should be red
+            setRGB(136,8,8)
             # display "PAUSED" on LCD
             setText("Spotify\nPAUSED")
         time.sleep(1)
